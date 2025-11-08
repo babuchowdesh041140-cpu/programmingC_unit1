77c6fb601c44e98170b237473c0b680989b8829e
babuchowdesh041140-cpu <babuchowdesh041140@gmail.com>
2025-11-08 09:40:51 +0530

    c programing
    c.p session 1 complited

---
+++ cp.2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a = 45;
+    float b = 12.66;
+    char c ='z';
+    printf("integer:%d\n",a);
+    printf("float(2decimals):%2f\n",b);
+    printf("float(5decimals):%5f\n",b);
+    printf("character:%c\n",c);
+    return 0;
+}
