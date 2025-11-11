ba86c545066540a7f098b5a670d5256365302f69
babuchowdesh041140-cpu <babuchowdesh041140@gmail.com>
2025-11-11 08:39:15 +0530

    c programing
    completed(11/11/2025)

---
+++ c23.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int age;
+    printf("enter your age:");
+    scanf("%d,&age");
+    if(age>=18)
+    printf("you are elegibleto vote\n");
+    else
+    printf("you are not eligible to vote\n");
+    return 0;
+}
