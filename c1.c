b161ad8d80595dd3919bc6588c4944c89eed681b
babuchowdesh041140-cpu <babuchowdesh041140@gmail.com>
2025-11-18 09:00:16 +0530

    c p unit 2
    completed(18/11/25)

--- c1.c
+++ c1.c
@@ -1,16 +1,7 @@
- #include <stdio.h>
- int main()
- {
- char name[20];
- int age;
- float marks;
- printf("Enter name: ");
- scanf("%s", name);
- printf("Enter age: ");
- scanf("%d", &age;);
- printf("Enter marks: ");
- scanf("%f", &marks;);
- printf("\nStudent Details:\n");
- printf("Name: %s\nAge: %d\nMarks: %.2f\n", name, age, marks)
- return 0;
- }
+#include<stdio.h>
+int main(){
+    int arr[]={10,20,30,40,50};
+    int count=sizeof(arr)/sizeof(arr[0]);
+    printf("total number of elements in the array=%d\n",count);
+    return 0;
+}
--- c3.c
+++ c3.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-int main()
- {
- float length, breadth, area, perimeter;
- printf("Enter length and breadth: ");
- scanf("%f %f", &length, &breadth);
- area = length * breadth;
- perimeter = 2 * (length + breadth);
- printf("Area: %.2f\n", area);
- printf("Perimeter: %.2f\n", perimeter);
- return 0;
- }
+#include<stdio.h>
+int main(){
+    int arr[6]={5,12,18,25,32,40};
+    printf("enter element to search:");
+    scanf("%d",&key);
+    while(low<=high){
+        mid=(low+high)/2;
+        if(arr[mid]==key){
+            printf("element%d found at position%d\n",key,mid+1);
+            return 0;
+        }
+    }
+}
